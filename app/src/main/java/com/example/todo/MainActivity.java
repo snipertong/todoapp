ff38232b9d294f42770c1583b309d8c8d5a799b0
snipertong <huntertonggg@gmail.com>
2022-06-12 16:19:12 +0800

    final(改变日历样式，增加todo事项在日历中日历标记)

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -13,7 +13,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
+//import android.widget.CalendarView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -25,16 +25,24 @@ import com.example.todo.adapter.CalendarAdapter;
 import com.example.todo.widget.ListItem;
 import com.example.todo.adapter.TodoCustomAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.haibin.calendarview.Calendar;
+import com.haibin.calendarview.CalendarView;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Calendar;
+//import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Context context;
     private CalendarView calendarView;
     private ListView listView;
     private ArrayList<ListItem> todos;
+    private ArrayList<ListItem> todo;
     private FloatingActionButton fab;
     private TextView date;
     private CalendarAdapter adapter;
@@ -42,6 +50,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private boolean flag=false;
     private String[] selectionArgs;
     private String string1,string;
+    private static String REGEX_CHINESE = "[\u4e00-\u9fa5]";
+    int year1,month1,day1;
 
 
     @Override
@@ -63,23 +73,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fab.setOnClickListener(this);
         navi=findViewById(R.id.navi);
         navi.setOnClickListener(this);
-        
-        //显示点击日期的item
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month1, int dayOfMonth) {
-//                int month = month1+1;
-//                date.setText(year+"年"+month+"月"+dayOfMonth+"日");
-//                String string1=date.getText().toString();
 
-
-            }
-        });
-        
-        final Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH)+1;
-        int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+        LocalDate now = LocalDate.now();
+        int year = now.getYear();
+        int month = now.getMonthValue();
+        int dayOfMonth = now.getDayOfMonth();
         date.setText(year+"年"+month+"月"+dayOfMonth+"日");
         string=date.getText().toString();
         selectionArgs = new  String[]{string};
@@ -108,8 +106,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         db.close();
         adapter=new CalendarAdapter(this,todos);
         listView.setAdapter(adapter);
+        dot();
         adapter.notifyDataSetChanged();
+
     }
+
+    protected void dot(){
+        todo = new ArrayList<>();
+        TodoOpenHelper todoOpenHelper = TodoOpenHelper.getInstance(getApplicationContext());
+        SQLiteDatabase db = todoOpenHelper.getReadableDatabase();
+        Cursor cursor = db.query(Contract.TODO_TABLE_NAME, null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+        do {
+        @SuppressLint("Range") String title = cursor.getString(cursor.getColumnIndex(Contract.TODO_TITLE));
+        @SuppressLint("Range") byte[] photo = cursor.getBlob(cursor.getColumnIndex(Contract.TODO_PHOTO));
+        @SuppressLint("Range") String content = cursor.getString(cursor.getColumnIndex(Contract.TODO_CONTENT));
+        @SuppressLint("Range") String date = cursor.getString(cursor.getColumnIndex(Contract.TODO_DATE));
+        @SuppressLint("Range") String end = cursor.getString(cursor.getColumnIndex(Contract.TODO_END));
+        @SuppressLint("Range") int status = cursor.getInt(cursor.getColumnIndex(Contract.TODO_STATUS));
+        @SuppressLint("Range") long id = cursor.getLong(cursor.getColumnIndex(Contract.TODO_ID));
+        ListItem listItem = new ListItem(title, content, id,date,status,end,photo);
+        todo.add(listItem);
+        }while (cursor.moveToNext());}
+        Map<String, Calendar> map = new HashMap<>();
+        for (ListItem listItem : todo){
+            String date = listItem.getDate();
+            if(date.length() >= 1) {
+                string = date.replaceAll(REGEX_CHINESE,"");
+                year1 = Integer.parseInt(string.substring(0, 4));
+                month1 = Integer.parseInt(string.charAt(4)+"");
+                day1 = Integer.parseInt(string.substring(5, 7));
+            }
+            map.put(getSchemeCalendar(year1, month1, day1, 0xFF40db25,"事").toString(),
+                    getSchemeCalendar(year1 ,month1, day1, 0xFF40db25,"事"));
+            calendarView.setSchemeDate(map);
+        }
+
+    }
+
+    private Calendar getSchemeCalendar(int year, int month, int day, int color, String text) {
+        Calendar calendar = new Calendar();
+        calendar.setYear(year);
+        calendar.setMonth(month);
+        calendar.setDay(day);
+        calendar.setSchemeColor(color);//如果单独标记颜色、则会使用这个颜色
+        calendar.setScheme(text);
+        return calendar;
+    }
+
 
     private void showPopupMenu(View view) {
         // View当前PopupMenu显示的相对View的位置
@@ -158,5 +202,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
+
+
+
+
+
 
 }
